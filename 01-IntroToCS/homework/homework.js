ea52ce2e11045a27b492ead5c5ee3d9dde63c53c
Kevin <kevinesteban2010@hotmail.com>
2022-08-02 14:13:20 -0500

    primera tarea m1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,14 +1,29 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  var numS = num.toString();
+  var result = 0
+  var exp = 0
+  for(var  i = numS.length-1 ; i>=0 ; i--){
+    if(numS[i] == "1"){
+      result = result + Math.pow(2, exp)
+    }
+    exp++
+  }
+  return result
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
+  var result = num
+  var mod = [];
 
+  while(result >= 1){
+    mod.push(result%2)
+    result=Math.floor(result/2)
+  }
+  return mod.reverse().toString().replace(/,/g,"")
 }
+
 
 
 module.exports = {
