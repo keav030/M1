353bad116c93f0be752b5ad8df4467cb0d555a5b
Kevin <kevinesteban2010@hotmail.com>
2022-09-05 15:38:19 -0500

    update 05

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -106,25 +106,42 @@ Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero
 */
 
 function HashTable() {
+  this.buckets = []
+  this.numBuckets = 35 
   
 }
 
 HashTable.prototype.hash = function(key){
+  let sum = 0
+  for (let i = 0; i < key.length; i++) {
+    sum = sum + key.charCodeAt(i)
+  }
+  return sum%this.numBuckets
 }
 
 HashTable.prototype.set = function(key, value){
+  if(typeof key !== 'string') throw TypeError('Keys must be strings')
+  let index = this.hash(key)
+  if(!this.buckets[index]) {
+    this.buckets[index]={}
+  }
+  this.buckets[index][key]= value
   
-
 }
 
 HashTable.prototype.get = function(key){ 
-  
-
+  let index = this.hash(key)
+  return this.buckets[index][key]
 }
 
 HashTable.prototype.hasKey = function(key){
-  
+  let index = this.hash(key)
+  return this.buckets[index].hasOwnProperty(key)
 }
+
+
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
