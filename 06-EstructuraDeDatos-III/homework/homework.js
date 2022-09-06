9d12cd51d5cf80bcee6e8d024961e0525b1fb0f7
Kevin <kevinesteban2010@hotmail.com>
2022-09-06 11:57:31 -0500

    update homework 06

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -22,94 +22,80 @@ function BinarySearchTree(value) {
 }
 
 BinarySearchTree.prototype.size = function(){
-  if(!this.left && !this.right)return 1
-  if(!this.left) return 1 + this.right.size()
-  if(!this.right) return 1 + this.left.size()
-  if(this.left && this.right) return 1 + this.right.size() + this.left.size()
-
+  let count = 1
+  if(this.left) count = count + this.left.size()
+  if(this.right) count = count + this.right.size()
+  return count
 }
 
 BinarySearchTree.prototype.insert = function(value){ 
-  if(!this.left && this.value>value){ 
-    this.left=new BinarySearchTree(value)
-  }else if(!this.right && this.value<value){ 
-    this.right=new BinarySearchTree(value)
-  }else{
-    if(this.value>value)return this.left.insert(value)
-    if(this.value<value)return this.right.insert(value)
-  }  
+  if (value < this.value) {
+    if (!this.left) {
+      this.left = new BinarySearchTree(value)
+    }else{
+      this.left.insert(value)
+    }
+  }
+  if(value > this.value){
+    if (!this.right) {
+      this.right = new BinarySearchTree(value)
+    }else{
+      this.right.insert(value)
+    }
+  }
 } 
 
 BinarySearchTree.prototype.contains = function(value){
-  if(this.value===value)return true
-  
+  if(value === this.value)return true 
+  if(value < this.value){
+    if(this.left){
+      return this.left.contains(value)
+    } 
+  }
   if(value > this.value){
-    if(!this.right){
-      return false
-    }else{
+    if(this.right){
       return this.right.contains(value)
     }
-  }else{
-    if(!this.left){
-      return false
-    }else{
-      return this.left.contains(value)
-    }
   }
+  return false
 }
-
 
 BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
-  if(!order || order === "in-order"){
-    this.left && this.left.depthFirstForEach(cb,order) // es como un if
+  if(order === "in-order" || order === undefined){
+    if(this.left)this.left.depthFirstForEach(cb, order)  
     cb(this.value)
-    this.right && this.right.depthFirstForEach(cb,order)
-  }else if(order === "post-order"){
-    this.left && this.left.depthFirstForEach(cb,order) // es como un if
-    this.right && this.right.depthFirstForEach(cb,order)
+    if(this.right)this.right.depthFirstForEach(cb, order)
+  }
+
+  if(order === "pre-order"){
     cb(this.value)
-  }else{
+    if(this.left)this.left.depthFirstForEach(cb, order)
+    if(this.right)this.right.depthFirstForEach(cb, order)
+  }
+  
+  if(order === "post-order"){
+    if(this.left)this.left.depthFirstForEach(cb, order)
+    if(this.right)this.right.depthFirstForEach(cb, order)
     cb(this.value)
-    this.left && this.left.depthFirstForEach(cb,order)
-    this.right && this.right.depthFirstForEach(cb,order)
   }
 }
 
-BinarySearchTree.prototype.breadthFirstForEach = function(cb, array = []){
+BinarySearchTree.prototype.breadthFirstForEach = function(cb, array=[]){
   if(this.left){
     array.push(this.left)
   }
   if(this.right){
     array.push(this.right)
   }
+
   cb(this.value)
-  if(array.length>0){
+
+  while(array.length > 0){
     array.shift().breadthFirstForEach(cb, array)
   }
 }
 
 
-
-//[15, 25, 5, 17, 21, 28, 0, 14, 50, 1, 45, 13, 12, 11, 30, 35, 33, 31, 34];
-/*
-const t = new BinarySearchTree(20)
-// console.log('//aqui primero//' , t)
-t.insert(15)
-// console.log('//aqui entra 15//:' , t)
-t.insert(25)
-t.insert(5)
-// console.log('//aqui entra 25//:' , t)
-t.insert(17)
-// console.log('//aqui entra 17//:' , t)
-t.insert(21)
-t.insert(28)
-
-console.log('//aqui entra 21//:' , t)
-console.log("=================================================================")
-
-
-console.log('//aqui entra 21//:' , t.contains(5))
-*/
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
